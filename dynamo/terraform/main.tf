742d597b1e58f2b807b2da20ae81b557f587fea0
steve.sakol <steve.sakol@appian.com>
2019-10-31 16:23:55 -0400

    added separate terraform specifically for dynamodb instance'

---
+++ dynamo/terraform/main.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "ja3-table" {
+  name = "JA3Fingerprints"
+  billing_mode = "PROVISIONED"
+  read_capacity = 5
+  write_capacity = 5
+  hash_key = "ja3"
+
+  attribute {
+    name = "ja3"
+    type = "S"
+
+  }
+
+  tags = {
+    Name = "JA3-Fingerprint-Table"
+    Environment = "ja3-production"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled = true
+  }
+}
